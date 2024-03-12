5b3986c6c8e0443455c95c624b9435f5c2cb2532
niebingcheng <766173427@qq.com>
2024-03-12 11:35:16 +0800

    1、保存修改，拉取最新代码

--- src/main/java/com/bear/reseeding/controller/UavController.java
+++ src/main/java/com/bear/reseeding/controller/UavController.java
@@ -1811,7 +1811,7 @@ public class UavController {
     //region 数据增删查改
 
     /**
-     * 查询飞行架次 queryFlightNumber
+     * 查询飞行架次 (前台查询一个月的飞行架次信息）
      *
      * @param uavId     无人机编号
      * @param startTime
@@ -1858,7 +1858,7 @@ public class UavController {
     }
 
     /**
-     * 实时拍摄照片表 queryPhotoInfo
+     * 架次查询关联的实时拍摄照片表
      *
      * @param uavId
      * @param eachsortieId
@@ -1883,7 +1883,7 @@ public class UavController {
     }
 
     /**
-     * 查询草原空洞表 queryHoleInfo
+     * 查询草原空洞表
      *
      * @param uavId        无人机编号
      * @param eachsortieId 飞行架次
